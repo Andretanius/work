b0b7196278d601cbdf6f3a8558b0058a3f712d0e
Andre <Andre>
2016-12-25 10:15:01 +0200

    new files

---
+++ script.js
@@ -0,0 +1,7 @@
+setTimeout(function(){
+ var h2tag = document.createElement("h2");
+ h2tag.innerHTML = "Владимир Дарий мой преподователь";
+ document.getElementsByTagName("h1")[0].appendChild(h2tag);
+}, 2000);
+
+
