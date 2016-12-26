fd2df86fd567a8164c5a7fea8c5be3d25619a91a
Andre <Andre>
2016-12-26 17:08:21 +0200

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 setTimeout(function(){
  var h2tag = document.createElement("h2");
- h2tag.innerHTML = "Владимир Дарий мой преподователь";
+ h2tag.innerHTML = "To be continued!";
  document.getElementsByTagName("h1")[0].appendChild(h2tag);
 }, 2000);
 
